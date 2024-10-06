105007bb1f9f4a2032bff9411989de408b891507
emsweetland <emsweetland200@gmail.com>
2024-10-06 16:42:56 -0500

    convert string to byte slice

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main //executable type package
 //package main requires function main, which will be called whenever we execute this file
+import "fmt"
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
-
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
